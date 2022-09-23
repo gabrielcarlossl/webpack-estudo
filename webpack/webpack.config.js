ed77dc0bedc92465e53a9c86b187d42dd1d1894d
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2022-09-23 09:34:14 -0300

    iniciado npm start

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,7 +2,6 @@ const webpack = require('webpack')
 
 // objeto que possui toda a configuração do webpack 
 module.exports = {
-
     mode: 'development', // informar se esta no modo desenvolvimento ou produção
-    entry: './src/principal.js',  // informar qual é a entrada do arquivo do webpack
+    entry: './src/principal.js'  // informar qual é a entrada do arquivo do webpack
 }
