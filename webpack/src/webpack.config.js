35b1107aa170637b7f5a49b086d86ff8166bda08
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2022-09-23 09:13:01 -0300

    estudando webpack

---
+++ webpack/src/webpack.config.js
@@ -0,0 +1,8 @@
+const webpack = require('webpack')
+
+// objeto que possui toda a configuração do webpack 
+module.exports = {
+
+    mode: 'development', // informar se esta no modo desenvolvimento ou produção
+    entry: './src/principal.js',  // informar qual é a entrada do arquivo do webpack
+}
